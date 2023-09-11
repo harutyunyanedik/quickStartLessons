e5e12970ac170a61230be8e1b497ab90190e11bd
musayelyan <musayelyan.art@gmail.com>
2023-09-11 13:54:59 +0400

    dejnde

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.quickstartlessons
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
 }
