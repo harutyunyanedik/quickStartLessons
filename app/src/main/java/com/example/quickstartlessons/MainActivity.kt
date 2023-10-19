2c8256760d23833694eb378dfb5409c62c18feb2
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2023-10-19 09:59:52 +0400

    first lesson

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.quickstartlessons
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+
+        }
     }
 }
