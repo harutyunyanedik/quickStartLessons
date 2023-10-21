164314b45f83f36914709c5231c161a89af5c16a
Diana Melkonay <dianamelkonyanit@gmail.com>
2023-10-21 20:24:49 +0400

    modified homework Calculator

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -1,71 +1,158 @@
 package com.example.quickstartlessons
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private val math_operation = findViewById<TextView>(R.id.math_operation)
-    private val btn_zero = findViewById<TextView>(R.id.btn_zero)
-    private val btn_one = findViewById<TextView>(R.id.btn_one)
-    private val btn_two = findViewById<TextView>(R.id.btn_two)
-    private val btn_three = findViewById<TextView>(R.id.btn_three)
-    private val btn_four = findViewById<TextView>(R.id.btn_four)
-    private val btn_five = findViewById<TextView>(R.id.btn_five)
-    private val btn_six = findViewById<TextView>(R.id.btn_six)
-    private val btn_seven = findViewById<TextView>(R.id.btn_seven)
-    private val btn_eight = findViewById<TextView>(R.id.btn_eight)
-    private val btn_nine = findViewById<TextView>(R.id.btn_nine)
 
-    private val btn_plus = findViewById<TextView>(R.id.btn_plus)
-    private val btn_minus = findViewById<TextView>(R.id.btn_minus)
-    private val btn_mult = findViewById<TextView>(R.id.btn_multiply)
-    private val btn_drop = findViewById<TextView>(R.id.btn_drop)
-    private val btn_bracket = findViewById<TextView>(R.id.btn_bracket)
-    private val btn_bracketclose = findViewById<TextView>(R.id.btn_brackets_close)
-    private val btn_dot = findViewById<TextView>(R.id.btn_dot)
+    private lateinit var btn_0: Button
+    private lateinit var btn_1: Button
+    private lateinit var btn_2: Button
+    private lateinit var btn_3: Button
+    private lateinit var btn_4: Button
+    private lateinit var btn_5: Button
+    private lateinit var btn_6: Button
+    private lateinit var btn_7: Button
+    private lateinit var btn_8: Button
+    private lateinit var btn_9: Button
+    private lateinit var btn_plus: Button
+    private lateinit var btn_minus: Button
+    private lateinit var btn_drop: Button
+    private lateinit var btn_multiply: Button
+    private lateinit var btn_dot: Button
+    private lateinit var btn_back: Button
+    private lateinit var btn_equals: Button
+    private lateinit var btn_math_operation: Button
+    private lateinit var btn_result_text: Button
+    private lateinit var btn_brackets: Button
+    private lateinit var btn_brackets_close: Button
+    private lateinit var btn_AC: Button
 
-    private val btn_AC = findViewById<TextView>(R.id.btn_AC)
-    private val btn_back = findViewById<TextView>(R.id.btn_back)
-    private val result_text = findViewById<TextView>(R.id.result_text)
+    private var currentOperand: Double = 0.0
+    private var currentOperator: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_linear)
-
-
-        btn_zero.setOnClickListener { showTextFields("0") }
-        btn_one.setOnClickListener { showTextFields("1") }
-        btn_two.setOnClickListener { showTextFields("2") }
-        btn_three.setOnClickListener { showTextFields("3") }
-        btn_four.setOnClickListener { showTextFields("4") }
-        btn_five.setOnClickListener { showTextFields("5") }
-        btn_six.setOnClickListener { showTextFields("6") }
-        btn_seven.setOnClickListener { showTextFields("7") }
-        btn_eight.setOnClickListener { showTextFields("8") }
-        btn_nine.setOnClickListener { showTextFields("9") }
-
-        btn_plus.setOnClickListener { showTextFields("+") }
-        btn_minus.setOnClickListener { showTextFields("-") }
-        btn_mult.setOnClickListener { showTextFields("*") }
-        btn_drop.setOnClickListener { showTextFields("/") }
-        btn_bracket.setOnClickListener { showTextFields("(") }
-        btn_bracketclose.setOnClickListener { showTextFields(")") }
-        btn_dot.setOnClickListener { showTextFields(".") }
-
-        btn_AC.setOnClickListener {
-            math_operation.text = ""
+        operashion()
+        btn_0.setOnClickListener {
+            numberClick("0")
+        }
+        btn_1.setOnClickListener {
+            numberClick("1")
+        }
+        btn_2.setOnClickListener {
+            numberClick("2")
+        }
+        btn_3.setOnClickListener {
+            numberClick("3")
+        }
+        btn_4.setOnClickListener {
+            numberClick("4")
+        }
+        btn_5.setOnClickListener {
+            numberClick("5")
+        }
+        btn_6.setOnClickListener {
+            numberClick("6")
+        }
+        btn_7.setOnClickListener {
+            numberClick("7")
+        }
+        btn_8.setOnClickListener {
+            numberClick("8")
+        }
+        btn_9.setOnClickListener {
+            numberClick("")
+        }
+        btn_plus.setOnClickListener {
+            operatorClick("+")
+        }
+        btn_minus.setOnClickListener {
+            operatorClick("-")
+        }
+        btn_drop.setOnClickListener {
+            operatorClick("/")
+        }
+        btn_multiply.setOnClickListener {
+            operatorClick("*")
+        }
+        btn_dot.setOnClickListener {
+            operatorClick(".")
+        }
+        btn_brackets.setOnClickListener {
+            operatorClick("(")
+        }
+        btn_brackets_close.setOnClickListener {
+            operatorClick(")")
         }
         btn_back.setOnClickListener {
-            val str = math_operation.text.toString()
+            val str = btn_result_text.text.toString()
             if (str.isEmpty()) {
-                math_operation.text = str.substring(0, str.length - 1)
+                btn_result_text.text = str.substring(0, str.length - 1)
             }
-            result_text.text = ""
         }
+
+        btn_equals.setOnClickListener {
+            calculateResult()
+        }
+        btn_AC.setOnClickListener {
+            clearCalculator()
+        }
+
     }
 
-    fun showTextFields(str: String) {
-        math_operation.append(str)
+    private fun operashion() {
+        btn_0 = findViewById(R.id.btn_zero)
+        btn_1 = findViewById(R.id.btn_one)
+        btn_2 = findViewById(R.id.btn_two)
+        btn_3 = findViewById(R.id.btn_three)
+        btn_4 = findViewById(R.id.btn_four)
+        btn_5 = findViewById(R.id.btn_five)
+        btn_6 = findViewById(R.id.btn_six)
+        btn_7 = findViewById(R.id.btn_seven)
+        btn_8 = findViewById(R.id.btn_eight)
+        btn_9 = findViewById(R.id.btn_nine)
+        btn_plus = findViewById(R.id.btn_plus)
+        btn_minus = findViewById(R.id.btn_minus)
+        btn_drop = findViewById(R.id.btn_drop)
+        btn_multiply = findViewById(R.id.btn_multiply)
+        btn_dot = findViewById(R.id.btn_dot)
+        btn_back = findViewById(R.id.btn_back)
+        btn_equals = findViewById(R.id.btn_equals)
+        btn_result_text = findViewById(R.id.result_text)
+        btn_brackets = findViewById(R.id.btn_bracket)
+        btn_brackets_close = findViewById(R.id.btn_brackets_close)
+        btn_AC = findViewById(R.id.btn_AC)
     }
 
+    private fun numberClick(number: String) {
+        val currentText = btn_result_text.text.toString()
+        btn_result_text.text = currentText + number
+    }
+
+    private fun operatorClick(operator: String) {
+        currentOperator = operator
+        currentOperand = btn_result_text.text.toString().toDouble()
+        btn_result_text.text = ""
+    }
+
+    private fun clearCalculator() {
+        btn_result_text.text = ""
+        currentOperand = 0.0
+        currentOperator = ""
+    }
+
+    fun calculateResult() {
+        val operand = btn_result_text.toString().toDouble()
+        var result = 0.0
+        when (currentOperator) {
+            "+" -> result = currentOperand + operand
+            "-" -> result = currentOperand + operand
+            "/" -> result = currentOperand + operand
+            "*" -> result = currentOperand + operand
+        }
+        btn_result_text.text = result.toString()
+    }
 }
