6999f2ac7d442c5bbdf94a757898680ae2128b71
Diana Melkonay <dianamelkonyanit@gmail.com>
2023-10-29 03:20:54 +0400

    Login page

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -1,154 +1,128 @@
 package com.example.quickstartlessons
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.quickstartlessons.databinding.ActivityMainBinding
+import com.example.quickstartlessons.databinding.DataLayoutBinding
+import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+private lateinit var binding: DataLayoutBinding
 
-    private lateinit var btn_0: Button
-    private lateinit var btn_1: Button
-    private lateinit var btn_2: Button
-    private lateinit var btn_3: Button
-    private lateinit var btn_4: Button
-    private lateinit var btn_5: Button
-    private lateinit var btn_6: Button
-    private lateinit var btn_7: Button
-    private lateinit var btn_8: Button
-    private lateinit var btn_9: Button
-    private lateinit var btn_plus: Button
-    private lateinit var btn_minus: Button
-    private lateinit var btn_drop: Button
-    private lateinit var btn_multiply: Button
-    private lateinit var btn_dot: Button
-    private lateinit var btn_back: Button
-    private lateinit var btn_equals: Button
-    private lateinit var btn_result_text: Button
-    private lateinit var btn_brackets: Button
-    private lateinit var btn_brackets_close: Button
-    private lateinit var btn_AC: Button
+//    private lateinit var btn_0: Button
+//    private lateinit var btn_1: Button
+//    private lateinit var btn_2: Button
+//    private lateinit var btn_3: Button
+//    private lateinit var btn_4: Button
+//    private lateinit var btn_5: Button
+//    private lateinit var btn_6: Button
+//    private lateinit var btn_7: Button
+//    private lateinit var btn_8: Button
+//    private lateinit var btn_9: Button
+//    private lateinit var btn_plus: Button
+//    private lateinit var btn_minus: Button
+//    private lateinit var btn_drop: Button
+//    private lateinit var btn_multiply: Button
+//    private lateinit var btn_dot: Button
+//    private lateinit var btn_back: Button
+//    private lateinit var btn_equals: Button
+//    private lateinit var result_text: TextView
+//    private lateinit var btn_brackets: Button
+//    private lateinit var btn_brackets_close: Button
+//    private lateinit var btn_AC: Button
 
-    private var currentOperand: Double = 0.0
-    private var currentOperator: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_linear)
-        operashion()
-        btn_0.setOnClickListener {
-            numberClick("0")
+        binding=DataBindingUtil.setContentView(this,R.layout.data_layout)
+        binding.password.setOnClickListener{
+           val intent=Intent(this,ActivityTwo::class.java)
+            startActivity(intent)
         }
-        btn_1.setOnClickListener {
-            numberClick("1")
-        }
-        btn_2.setOnClickListener {
-            numberClick("2")
-        }
-        btn_3.setOnClickListener {
-            numberClick("3")
-        }
-        btn_4.setOnClickListener {
-            numberClick("4")
-        }
-        btn_5.setOnClickListener {
-            numberClick("5")
-        }
-        btn_6.setOnClickListener {
-            numberClick("6")
-        }
-        btn_7.setOnClickListener {
-            numberClick("7")
-        }
-        btn_8.setOnClickListener {
-            numberClick("8")
-        }
-        btn_9.setOnClickListener {
-            numberClick("")
-        }
-        btn_plus.setOnClickListener {
-            operatorClick("+")
-        }
-        btn_minus.setOnClickListener {
-            operatorClick("-")
-        }
-        btn_drop.setOnClickListener {
-            operatorClick("/")
-        }
-        btn_multiply.setOnClickListener {
-            operatorClick("*")
-        }
-        btn_dot.setOnClickListener {
-            operatorClick(".")
-        }
-        btn_brackets.setOnClickListener {
-            operatorClick("(")
-        }
-        btn_brackets_close.setOnClickListener {
-            operatorClick(")")
-        }
-        btn_back.setOnClickListener {
-            val str = btn_result_text.text.toString()
-            if (str.isEmpty()) {
-                btn_result_text.text = str.substring(0, str.length - 1)
-            }
-        }
-        btn_equals.setOnClickListener {
-            calculateResult()
-        }
-        btn_AC.setOnClickListener {
-            clearCalculator()
-        }
-    }
-    fun operashion() {
-        btn_0 = findViewById(R.id.btn_zero)
-        btn_1 = findViewById(R.id.btn_one)
-        btn_2 = findViewById(R.id.btn_two)
-        btn_3 = findViewById(R.id.btn_three)
-        btn_4 = findViewById(R.id.btn_four)
-        btn_5 = findViewById(R.id.btn_five)
-        btn_6 = findViewById(R.id.btn_six)
-        btn_7 = findViewById(R.id.btn_seven)
-        btn_8 = findViewById(R.id.btn_eight)
-        btn_9 = findViewById(R.id.btn_nine)
-        btn_plus = findViewById(R.id.btn_plus)
-        btn_minus = findViewById(R.id.btn_minus)
-        btn_drop = findViewById(R.id.btn_drop)
-        btn_multiply = findViewById(R.id.btn_multiply)
-        btn_dot = findViewById(R.id.btn_dot)
-        btn_back = findViewById(R.id.btn_back)
-        btn_equals = findViewById(R.id.btn_equals)
-        btn_result_text = findViewById(R.id.result_text)
-        btn_brackets = findViewById(R.id.btn_bracket)
-        btn_brackets_close = findViewById(R.id.btn_brackets_close)
-        btn_AC = findViewById(R.id.btn_AC)
-    }
 
-    fun numberClick(number: String) {
-        val currentText = btn_result_text.text.toString()
-        btn_result_text.text = buildString {
-            append(currentText)
-            append(number)
-        }
-    }
 
-    fun operatorClick(operator: String) {
-        currentOperator = operator
-        currentOperand = btn_result_text.text.toString().toDouble()
-        btn_result_text.text = ""
-    }
-    fun clearCalculator() {
-        btn_result_text.text = ""
-        currentOperand = 0.0
-        currentOperator = ""
-    }
-    fun calculateResult() {
-        val operand = btn_result_text.toString().toDouble()
-        var result = 0.0
-        when (currentOperator) {
-            "+" -> result = currentOperand + operand
-            "-" -> result = currentOperand + operand
-            "/" -> result = currentOperand + operand
-            "*" -> result = currentOperand + operand
-        }
-        btn_result_text.text = result.toString()
-    }
+//               operashion()
+//        btn_0.setOnClickListener { settextFind("0") }
+//        btn_1.setOnClickListener { settextFind("1") }
+//        btn_2.setOnClickListener { settextFind("2") }
+//        btn_3.setOnClickListener { settextFind("3") }
+//        btn_4.setOnClickListener { settextFind("4") }
+//        btn_5.setOnClickListener { settextFind("5") }
+//        btn_6.setOnClickListener { settextFind("6") }
+//        btn_7.setOnClickListener { settextFind("7") }
+//        btn_8.setOnClickListener { settextFind("8") }
+//        btn_9.setOnClickListener { settextFind("9") }
+//
+//
+//        btn_plus.setOnClickListener { settextFind("+") }
+//        btn_minus.setOnClickListener { settextFind("-") }
+//        btn_drop.setOnClickListener { settextFind("/") }
+//        btn_multiply.setOnClickListener { settextFind("*") }
+//        btn_dot.setOnClickListener { settextFind(".") }
+//        btn_brackets.setOnClickListener { settextFind("(") }
+//        btn_brackets_close.setOnClickListener { settextFind(")") }
+//        btn_equals.setOnClickListener {
+//            try {
+//                val ex = ExpressionBuilder(result_text.text.toString()).build()
+//                val result = ex.evaluate()
+//
+//                val longRes=result.toLong()
+//                if(result==longRes.toDouble()){
+//                    result_text.text=longRes.toString()
+//                }else
+//                    result_text.text=result.toString()
+//            } catch (e: Exception) {
+//                Log.d("Error", "message: ${e.message}")
+//            }
+//        }
+//
+//        btn_back.setOnClickListener {
+//            val str = result_text.text.toString()
+//            if (str.isEmpty()) {
+//                result_text.text = str.substring(0, str.length - 1)
+//                result_text.text = ""
+//            }
+//        }
+//        btn_AC.setOnClickListener { clearCalculator()}
+//
+//    }
+//
+//    fun operashion() {
+//        btn_0 = findViewById(R.id.btn_zero)
+//        btn_1 = findViewById(R.id.btn_one)
+//        btn_2 = findViewById(R.id.btn_two)
+//        btn_3 = findViewById(R.id.btn_three)
+//        btn_4 = findViewById(R.id.btn_four)
+//        btn_5 = findViewById(R.id.btn_five)
+//        btn_6 = findViewById(R.id.btn_six)
+//        btn_7 = findViewById(R.id.btn_seven)
+//        btn_8 = findViewById(R.id.btn_eight)
+//        btn_9 = findViewById(R.id.btn_nine)
+//        btn_plus = findViewById(R.id.btn_plus)
+//        btn_minus = findViewById(R.id.btn_minus)
+//        btn_drop = findViewById(R.id.btn_drop)
+//        btn_multiply = findViewById(R.id.btn_multiply)
+//        btn_dot = findViewById(R.id.btn_dot)
+//        btn_back = findViewById(R.id.btn_back)
+//        btn_equals = findViewById(R.id.btn_equals)
+//        result_text = findViewById(R.id.result_text)
+//        btn_brackets = findViewById(R.id.btn_bracket)
+//        btn_brackets_close = findViewById(R.id.btn_brackets_close)
+//        btn_AC = findViewById(R.id.btn_AC)
+//        btn_equals = findViewById(R.id.btn_equals)
+//    }
+//
+//    fun settextFind(str: String) {
+//        result_text.append(str)
+//    }
+//
+//    fun clearCalculator() {
+//        result_text.text = ""
+   }
+
 }
