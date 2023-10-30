0449929a5624ac3da3f1f19bebac17712a295f92
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2023-10-30 18:43:58 +0400

    additional

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -5,8 +5,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.webkit.WebViewClient
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -17,19 +15,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
-
-    }
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.button2.setOnClickListener {
-            binding.webView.settings.javaScriptEnabled = true
-            binding.webView.webViewClient = WebViewClient()
-            binding.webView.loadUrl("https://www.youtube.com/watch?v=fMTlLM58xGA")
+
         }
     }
 
