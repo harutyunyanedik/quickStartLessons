cb85edc7a6034b4500c29125b01f27467511bb6f
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2023-10-31 20:39:52 +0400

    additional

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -5,15 +5,23 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.webkit.WebViewClient
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.example.quickstartlessons.android.getUserNameFromPref
+import com.example.quickstartlessons.android.putUserNameToPref
 import com.example.quickstartlessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) {
+//        binding.image.setImageURI(it)
+    }
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +29,25 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.button2.setOnClickListener {
+            binding.webView.settings.javaScriptEnabled = true
+            binding.webView.webViewClient = WebViewClient()
+            binding.webView.loadUrl("https://www.youtube.com/")
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                ACTIVITY_TWO_REQUEST_CODE -> {
+                    val resultValue = data?.getStringExtra("key")
+                    binding.editText.setText(resultValue)
+                }
+
+                IMAGE_CHOOSER_REQUEST_CODE -> {
+
+                }
+            }
         }
     }
 
@@ -58,11 +84,17 @@ class MainActivity : AppCompatActivity() {
         val i = Intent()
         i.type = "image/*"
         i.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(i, "Select Picture"), 11)
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), IMAGE_CHOOSER_REQUEST_CODE)
     }
 
     private fun openBrowser() {
         val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
         startActivity(browserIntent)
+    }
+
+    companion object {
+        const val ACTIVITY_EXTRA_KEY = "activity_extra_key"
+        const val ACTIVITY_TWO_REQUEST_CODE = 200
+        const val IMAGE_CHOOSER_REQUEST_CODE = 11
     }
 }
