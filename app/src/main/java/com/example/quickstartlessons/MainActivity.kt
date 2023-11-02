8112d25f36c94c2d771980c345cd0aeaed4b329d
admin <anihpoghosyan@gmail.com>
2023-11-02 17:20:20 +0400

    shared prefs

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -1,23 +1,62 @@
 package com.example.quickstartlessons
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+
 import com.example.quickstartlessons.android.ActivityTwo
-import com.example.quickstartlessons.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.sign_in_page)
 
-        binding.button2.setOnClickListener {
-            val intent = Intent(this, ActivityTwo::class.java)
-            startActivity(intent)
+        val btnusername = findViewById<EditText>(R.id.username)
+        val btnpassword = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login_button)
+
+        val sharedpreferences = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
+        val getUsername = sharedpreferences.getString("USERNAME", "")
+        val getPassword = sharedpreferences.getString("PASSWORD", "")
+
+
+        if (getUsername != "" && getPassword != "") {
+            val i = Intent(this, ActivityTwo::class.java)
+        } else {
+            Toast.makeText(this, "Incorrect Login Details", Toast.LENGTH_LONG).show();
         }
+
+        login.setOnClickListener {
+            val username = btnusername.text.toString()
+            val password = btnpassword.text.toString()
+
+            val editor = sharedpreferences.edit()
+            editor.putString("USERNAME", username)
+            editor.putString("PASSWORD", password)
+            editor.apply()
+
+            val i = Intent(this, ActivityTwo::class.java)
+            startActivity(i)
+        }
+
     }
 }
+
+
+
+
+
+
+
+
