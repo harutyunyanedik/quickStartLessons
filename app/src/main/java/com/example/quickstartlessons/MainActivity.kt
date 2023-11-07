904a5895aca27e21b41c7fcf5cd5d7e5dddc19cd
Diana Melkonay <dianamelkonyanit@gmail.com>
2023-11-07 13:12:54 +0400

    homework new recycler adapter

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -4,20 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.quickstartlessons.android.ActivityTwo
+import com.example.quickstartlessons.android.FirstRecyclerAdapter
+import com.example.quickstartlessons.android.Model
 import com.example.quickstartlessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val adapter = FirstRecyclerAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.button2.setOnClickListener {
-            val intent = Intent(this, ActivityTwo::class.java)
-            startActivity(intent)
+        setupRecycleView()
+    }
+
+    fun setupRecycleView() {
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = adapter
+
+        adapter.updateData(createData())
+    }
+
+    fun createData(): List<Model> {
+        val list = mutableListOf<Model>()
+        for (i in 0..100) {
+            list.add(Model("Arsenal FS Specials"))
         }
+        return list
     }
 }
