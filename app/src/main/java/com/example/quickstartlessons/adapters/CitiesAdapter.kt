f1e53efbe544b4172d3345b639a824a994fc3408
MariamDavtyan2023 <davtyan_mariam91@mail.ru>
2023-11-19 14:20:37 +0400

    Creating project

---
+++ app/src/main/java/com/example/quickstartlessons/adapters/CitiesAdapter.kt
@@ -0,0 +1,90 @@
+package com.example.quickstartlessons.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quickstartlessons.databinding.DescriptionItemBinding
+import com.example.quickstartlessons.databinding.HeaderItemBinding
+import com.example.quickstartlessons.databinding.TitleItemBinding
+import com.example.quickstartlessons.models.RecyclerViewModel
+
+class CitiesAdapter : RecyclerView.Adapter<CitiesAdapter.BaseViewHolder>() {
+
+    private lateinit var inflater: LayoutInflater
+    private lateinit var context: Context
+
+    private val items = mutableListOf<RecyclerViewModel>()
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        context = recyclerView.context
+        inflater = LayoutInflater.from(context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return when (viewType) {
+            0 -> HeaderViewHolder(HeaderItemBinding.inflate(inflater, parent, false))
+            1 -> TitleViewHolder(TitleItemBinding.inflate(inflater, parent, false))
+            else -> DescriptionViewHolder(DescriptionItemBinding.inflate(inflater, parent, false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun getItemViewType(position: Int): Int {
+        return if (items[position].city == null && items[position].title == null) {
+            2
+        } else if (items[position].city == null && items[position].description == null) {
+            1
+        } else {
+            0
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateAdapter(list: List<RecyclerViewModel>?) {
+        items.clear()
+        list?.let {
+            items.addAll(it)
+        }
+        notifyDataSetChanged()
+    }
+
+    abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view.rootView) {
+        abstract fun bind(item: RecyclerViewModel)
+    }
+
+    inner class HeaderViewHolder(private val binding: HeaderItemBinding) :
+        BaseViewHolder(binding.root) {
+        override fun bind(item: RecyclerViewModel) {
+            binding.textViewHeader.text = item.city
+        }
+    }
+
+    inner class TitleViewHolder(private val binding: TitleItemBinding) :
+        BaseViewHolder(binding.root) {
+        override fun bind(item: RecyclerViewModel) {
+            binding.textViewTitle.text = item.title
+        }
+    }
+
+    inner class DescriptionViewHolder(private val binding: DescriptionItemBinding) :
+        BaseViewHolder(binding.root) {
+        override fun bind(item: RecyclerViewModel) {
+            binding.textViewDescription.text = item.description
+        }
+    }
+
+    companion object {
+        const val HEADER = 0
+        const val TITLE = 1
+        const val DESCRIPTION = 2
+    }
+}
---
+++ app/src/main/java/com/example/quickstartlessons/models/RecyclerViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.quickstartlessons.models
+
+data class RecyclerViewModel(
+    val city: String? = null,
+    val title: String? = null,
+    val description: String? = null
+)
