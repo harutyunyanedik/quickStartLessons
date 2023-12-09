f8b655ebeba34d51cecd89754ed4e8a24d30b410
MelineGogjyan <96750352+MelineGogjyan@users.noreply.github.com>
2023-12-10 00:07:41 +0400

    Sign in, Forgot password view pages
    Took 3 hours 11 minutes

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -8,6 +8,10 @@ import com.example.quickstartlessons.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var emailAdress: String = "empty"
+    private var password: String = "empty"
+    private var phoneNumber: String = "empty"
+    private var imageview: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
