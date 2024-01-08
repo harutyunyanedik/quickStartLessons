e522674f131486bf40209e480a755dc4a59d0d17
User <lusbars1991@mail.ru>
2024-01-08 19:55:27 +0400

    splash

--- app/src/main/java/com/example/quickstartlessons/module/launch/SplashActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/module/launch/SplashActivity.kt
@@ -2,11 +2,15 @@ package com.example.quickstartlessons.module.launch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.quickstartlessons.R
+import com.example.quickstartlessons.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+       binding=DataBindingUtil.setContentView(this,R.layout.activity_splash)
+
     }
 }
