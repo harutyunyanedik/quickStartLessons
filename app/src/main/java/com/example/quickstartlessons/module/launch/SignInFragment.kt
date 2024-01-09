dcdd72ce06584580566a6da05308d22addfc601f
MariamDavtyan2023 <davtyan_mariam91@mail.ru>
2024-01-09 16:56:52 +0400

    Update project

--- app/src/main/java/com/example/quickstartlessons/module/launch/SignInFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/launch/SignInFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.quickstartlessons.R
+import com.example.quickstartlessons.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
