601c6b96c5dbdb4338530236e47a6552923fa5f1
User <lusbars1991@mail.ru>
2024-01-10 19:28:57 +0400

    nav_graph_SplashActivity

--- app/src/main/java/com/example/quickstartlessons/MainActivity.kt
+++ app/src/main/java/com/example/quickstartlessons/MainActivity.kt
@@ -15,8 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-      // val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-      // val navController = navHostFragment.navController
-      // binding.bottomNavigationView.setupWithNavController(navController)
+       val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+       val navController = navHostFragment.navController
+       binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
