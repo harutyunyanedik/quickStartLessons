f47882aa42cc0861f2509be3f55f52d44fa94ff4
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2024-01-12 11:32:07 +0400

    review

--- app/src/main/java/com/example/quickstartlessons/module/signin/SignInFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/signin/SignInFragment.kt
@@ -1,11 +1,12 @@
 package com.example.quickstartlessons.module.signin
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.quickstartlessons.R
@@ -34,66 +35,18 @@ class SignInFragment : Fragment() {
             findNavController().navigate(R.id.action_signInFragment_to_resetPasswordViewPagerFragment)
         }
 
-        binding.emailEditText.doAfterTextChanged {
-            binding.signInButton.isEnabled = !it.isNullOrEmpty() && !binding.passwordEditText.text.isNullOrEmpty()
+        binding.emailEditText.setOnFocusChangeListener { view, isFocused ->
+            val text = (view as EditText).text.toString()
+            if (!isFocused)
+                if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                    binding.emailUsernameInputLayout.error = "invalid data"
+                }
         }
 
-        binding.passwordEditText.doAfterTextChanged {
-            binding.signInButton.isEnabled = !it.isNullOrEmpty() && !binding.emailEditText.text.isNullOrEmpty()
-        }
-
-         binding.emailEditText.doAfterTextChanged {
-             if(! Patterns.EMAIL_ADDRESS.matcher(it.toString()).matches() || it!!.matches(".*[0-9].*".toRegex())){
-                 binding.emailUsernameInputLayout.error="Invalid Email address or username "
-             }else{
-                 binding.emailUsernameInputLayout.error=null
-             }
-         }
-        binding.passwordEditText.doAfterTextChanged {
-            if(it!!.matches(".*[0-9].*".toRegex())){
-                binding.passwordInputLayout.error="Password must contain at least one number"
-            }
-            if(it.length<6){
-                binding.passwordInputLayout.error=" Password must be longer than 6 characters"
-
-            }else{
-                binding.emailUsernameInputLayout.error=null
-            }
+        binding.passwordEditText.setOnFocusChangeListener { view, isFocused ->
+            val text = (view as EditText).text.toString()
+            if (!isFocused)
+                (!TextUtils.isEmpty(text) && Patterns.EMAIL_ADDRESS.matcher(text).matches())
         }
     }
-
-
- //  private fun helperText() {
- //      binding.emailEditText.setOnFocusChangeListener { _, focus ->
- //          if (!focus) {
- //              binding.emailUsernameInputLayout.helperText = validEmailText()
- //          }
- //      }
- //      binding.passwordEditText.setOnFocusChangeListener{_, focus->
- //          if(!focus){
- //              binding.passwordInputLayout.helperText=validPassword()
- //          }
-
- //      }
- //  }
-
- //  private fun validEmailText(): String? {
- //      val emailText = binding.emailEditText.text.toString()
- //      if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()|| emailText.matches(".*[0-9].*".toRegex())) {
- //          return "Invalid Email address"
- //      }
-
- //      return null
- //  }
-
- //  private fun validPassword(): String? {
- //      val passwordText = binding.passwordEditText.text.toString()
- //      if (passwordText.length < 6) {
- //          return "Password must be longer than 6 characters"
- //      }
- //      if(!passwordText.matches(".*[0-9].*".toRegex())){
- //          return "Password must contain at least one number"
- //      }
- //      return null
- //  }
 }
