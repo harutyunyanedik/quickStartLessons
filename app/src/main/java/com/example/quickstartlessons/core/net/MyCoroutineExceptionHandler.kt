d8c2e1d6cbcaee3c1c017888d84942f8008b72d9
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2024-01-12 19:01:26 +0400

    base impl

--- app/src/main/java/com/example/quickstartlessons/core/net/MyCoroutineExceptionHandler.kt
+++ app/src/main/java/com/example/quickstartlessons/core/net/MyCoroutineExceptionHandler.kt
@@ -1,6 +1,7 @@
 package com.example.quickstartlessons.core.net
 
 import android.util.Log
+import com.example.quickstartlessons.module.base.fragment.BaseFragment
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlin.coroutines.CoroutineContext
 
@@ -9,7 +10,7 @@ class MyCoroutineExceptionHandler(override val key: CoroutineContext.Key<*>, pri
     override fun handleException(context: CoroutineContext, exception: Throwable) {
         try {
             Log.e("Global error", exception.localizedMessage ?: "")
-//            BaseFragment.removeLoader()
+            BaseFragment.removeLoader()
             resultCallBack?.onError()
         } catch (ex: Exception) {
             Log.e("Coroutine Exception", ex.localizedMessage ?: "")
