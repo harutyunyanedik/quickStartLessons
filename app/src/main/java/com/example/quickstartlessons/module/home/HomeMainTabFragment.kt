24d8ca4d8f85e29cc532bfde3f56310730a0e182
User <lusbars1991@mail.ru>
2024-01-15 19:57:29 +0400

    project xml

--- app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.quickstartlessons.R
+import com.example.quickstartlessons.databinding.ItemHomeMainTabBinding
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
 
 class HomeMainTabFragment : BaseFragment() {
-
+  private lateinit var binding:ItemHomeMainTabBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home_main_tab, container, false)
+        return inflater.inflate(R.layout.item_home_main_tab, container, false)
     }
 }
