5e99870f4d81478c455ba2905d12f341c6fc3d3c
User <lusbars1991@mail.ru>
2024-01-15 11:23:15 +0400

    email, password condition

--- app/src/main/java/com/example/quickstartlessons/module/signin/SignInFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/signin/SignInFragment.kt
@@ -1,14 +1,17 @@
 package com.example.quickstartlessons.module.signin
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
+
 import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.quickstartlessons.MainActivity
 import com.example.quickstartlessons.R
 import com.example.quickstartlessons.databinding.FragmentSignInBinding
 
@@ -30,23 +33,39 @@ class SignInFragment : Fragment() {
 
     }
 
+    @SuppressLint("SuspiciousIndentation")
     private fun setupViews() {
+
         binding.forgotPasswordTextView.setOnClickListener {
             findNavController().navigate(R.id.action_signInFragment_to_resetPasswordViewPagerFragment)
         }
 
-        binding.emailEditText.setOnFocusChangeListener { view, isFocused ->
-            val text = (view as EditText).text.toString()
-            if (!isFocused)
-                if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
-                    binding.emailUsernameInputLayout.error = "Invalid Email address or username"
-                }
-        }
+        binding.emailEditText.doAfterTextChanged {
+            val userNameText = it.toString()
 
-        binding.passwordEditText.setOnFocusChangeListener { view, isFocused ->
-            val text = (view as EditText).text.toString()
-            if (!isFocused)
-                (!TextUtils.isEmpty(text) && Patterns.EMAIL_ADDRESS.matcher(text).matches())
+            binding.passwordEditText.doAfterTextChanged { text ->
+                val passwordText = text.toString()
+
+                binding.signInButton.setOnClickListener {
+                    when {
+                        !Patterns.EMAIL_ADDRESS.matcher(userNameText).matches() -> {
+                            binding.emailUsernameInputLayout.error = "Invalid Email address or username"
+
+                        }
+
+                        passwordText.length < 6 -> {
+                            binding.passwordEditText.error = "Password must be 6 or more characters"
+                        }
+
+                        else ->
+                            startActivity(Intent(requireContext(), MainActivity::class.java))
+
+                    }
+                }
+            }
         }
     }
+
 }
+
+
