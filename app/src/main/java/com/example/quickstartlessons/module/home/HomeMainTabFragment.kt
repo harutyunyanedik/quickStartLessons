29fa9292b570321d616e179320a2fb7a882a1bea
User <lusbars1991@mail.ru>
2024-01-17 11:10:52 +0400

    Product

--- app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
@@ -5,19 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.quickstartlessons.R
 import com.example.quickstartlessons.databinding.ItemHomeMainTabBinding
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
 import com.example.quickstartlessons.module.product.ProductAdapter
 import com.example.quickstartlessons.module.product.ProductViewModel
-import kotlinx.coroutines.launch
+
 
 class HomeMainTabFragment : BaseFragment() {
   private lateinit var binding:ItemHomeMainTabBinding
-  private lateinit var adapter: ProductAdapter
+  private lateinit var  adapter:ProductAdapter
   private val viewModel:ProductViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,9 +26,12 @@ class HomeMainTabFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         setupView()
+
     }
     private fun setupView(){
+        viewModel.getProduct()
         binding.recyclerView.adapter=adapter
         binding.recyclerView.layoutManager=GridLayoutManager(requireContext(),2)
         viewModel.productLiveData.observe(viewLifecycleOwner){
--- app/src/main/java/com/example/quickstartlessons/module/product/ProductAdapter.kt
+++ app/src/main/java/com/example/quickstartlessons/module/product/ProductAdapter.kt
@@ -10,7 +10,7 @@ import com.example.quickstartlessons.core.net.ProductData
 import com.example.quickstartlessons.databinding.FragmentProductDataBinding
 
 
-class ProductAdapter:RecyclerView.Adapter <ProductAdapter.ProductViewHolder>(){
+class ProductAdapter(private val onItemClick:(String,String)->Unit):RecyclerView.Adapter <ProductAdapter.ProductViewHolder>(){
    private lateinit var inflater:LayoutInflater
    private lateinit var context:Context
    private lateinit var  items:MutableList<ProductData>
@@ -39,8 +39,23 @@ class ProductAdapter:RecyclerView.Adapter <ProductAdapter.ProductViewHolder>(){
         notifyDataSetChanged()
     }
     inner class ProductViewHolder(private val binding:FragmentProductDataBinding):RecyclerView.ViewHolder(binding.root){
+        init {
+            binding.favoriteProduct.setOnClickListener {
+                if (it.isPressed) {
+                    if (adapterPosition != RecyclerView.NO_POSITION) {
+                        onItemClick.invoke(items[adapterPosition].thumbnail, items[adapterPosition].title)
+                        notifyItemChanged(adapterPosition)
+                    }
+                }
+            }
+        }
+
        fun bind(item:ProductData){
          Glide.with(context).load(item.thumbnail).into(binding.imageProduct)
+         binding.productBrand.text=item.brand
+           binding.productPrice.text=item.price.toString()
+           binding.productTitle.text=item.title
+           binding.productDescription.text=item.description
 
        }
     }
