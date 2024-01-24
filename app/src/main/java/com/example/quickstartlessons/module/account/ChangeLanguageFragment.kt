13e826f1fc35b468b491f86f0f559cbf4260e9f8
User <lusbars1991@mail.ru>
2024-01-24 16:47:39 +0400

    change language

--- app/src/main/java/com/example/quickstartlessons/module/account/ChangeLanguageFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/account/ChangeLanguageFragment.kt
@@ -5,22 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.example.quickstartlessons.R
+import com.example.quickstartlessons.databinding.FragmentChangeLangugaeBinding
 
-class ChangeLanguageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+class ChangeLanguageFragment : DialogFragment() {
+    private lateinit var binding:FragmentChangeLangugaeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_langugae, container, false)
+       binding=FragmentChangeLangugaeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
 }
