0b07250d26b0ab18a16470e9b00c0732de743034
admin <anihpoghosyan@gmail.com>
2024-01-24 17:53:32 +0400

    account page

--- app/src/main/java/com/example/quickstartlessons/module/account/AccountMainTabFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/account/AccountMainTabFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.quickstartlessons.R
 import com.example.quickstartlessons.databinding.FragmentAccountMainTabBinding
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
 
@@ -23,9 +25,9 @@ class AccountMainTabFragment : BaseFragment() {
         setUpViews()
     }
 
-    fun setUpViews(){
-        binding.settingsIcon.setOnClickListener{
-
+    private fun setUpViews() {
+        binding.settingsIcon.setOnClickListener {
+           findNavController().navigate(R.id.action_accountFakeFragment_to_settingsFragment)
         }
     }
 
