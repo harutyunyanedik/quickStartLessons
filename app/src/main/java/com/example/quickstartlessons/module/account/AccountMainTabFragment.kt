30709089e3d6aa7328de2eab8d212cf3d45fc792
User <lusbars1991@mail.ru>
2024-01-24 17:38:45 +0400

    change language

--- app/src/main/java/com/example/quickstartlessons/module/account/AccountMainTabFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/account/AccountMainTabFragment.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.quickstartlessons.R
 import com.example.quickstartlessons.databinding.FragmentAccountMainTabBinding
 import com.example.quickstartlessons.databinding.FragmentHomeMainTabBinding
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
 
 class AccountMainTabFragment : BaseFragment() {
-       private lateinit var binding: FragmentAccountMainTabBinding
+    private lateinit var binding: FragmentAccountMainTabBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       binding=FragmentAccountMainTabBinding.inflate(inflater, container, false)
+        binding = FragmentAccountMainTabBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -24,8 +25,7 @@ class AccountMainTabFragment : BaseFragment() {
         binding.settings.setOnClickListener {
 
         }
-          }
-
+    }
 
 
 }
