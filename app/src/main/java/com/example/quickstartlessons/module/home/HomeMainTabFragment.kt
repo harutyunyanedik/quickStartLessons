45d2e051a98d86cecb7b6616d73c51f7a8a7203c
edikharutyunyan <edikharutyunyan edik.harutyunyan@betconstruct.com>
2024-01-26 11:07:42 +0400

    language change

--- app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/home/HomeMainTabFragment.kt
@@ -5,17 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.quickstartlessons.R
-import com.example.quickstartlessons.module.albums.presentation.AlbumsViewModel
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeMainTabFragment : BaseFragment() {
 
-    private val viewModel by viewModel<AlbumsViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.getAlbums()
     }
 
     override fun onCreateView(
