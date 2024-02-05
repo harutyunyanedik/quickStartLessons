cb79246c4c7fc487f83f430b8a3429f874f0c162
admin <anihpoghosyan@gmail.com>
2024-02-05 17:29:38 +0400

    sign in

--- app/src/main/java/com/example/quickstartlessons/module/home/ui/SearchFragment.kt
+++ app/src/main/java/com/example/quickstartlessons/module/home/ui/SearchFragment.kt
@@ -14,6 +14,7 @@ import com.example.quickstartlessons.databinding.FragmentSearchBinding
 import com.example.quickstartlessons.module.base.fragment.BaseFragment
 import com.example.quickstartlessons.module.home.ui.adapters.ProductsAdapter
 import com.example.quickstartlessons.module.products.details.ProductDetailsFragmentDirections
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.android.ext.android.inject
 
 
